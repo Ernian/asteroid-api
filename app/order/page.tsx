c247413b2bba5f0a49ad9a9a4d4bfb216e0db690
light <estarosa@list.ru>
2023-08-17 23:58:39 +0300

    empty order case

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -9,16 +9,21 @@ const OrderPage = () => {
   return (
     <>
       <section className={css.container}>
-        <h2 className={css.title}>Заказ отправлен!</h2>
         {
-          state.order.map(asteroid => (
-            <AsteroidCard
-              key={asteroid.id}
-              asteroid={asteroid}
-              isKm={state.isKm}
-              showBtn={false}
-            />
-          ))
+          state.order.length > 0 &&
+          <h2 className={css.title}>Заказ отправлен!</h2>
+        }
+        {
+          state.order.length > 0 ?
+            state.order.map(asteroid => (
+              <AsteroidCard
+                key={asteroid.id}
+                asteroid={asteroid}
+                isKm={state.isKm}
+                showBtn={false}
+              />
+            )) :
+            <h2 className={css.title}>Заказ пуст!</h2>
         }
       </section>
       <footer className={css.footer}>&copy; Все права и планета защищены</footer>
